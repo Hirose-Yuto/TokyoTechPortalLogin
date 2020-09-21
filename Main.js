b17c2c70266a8605268e841503e83d8a83dce18e
Hirose-Yuto <hirose.y.aj@m.titech.ac.jp>
2020-09-21 17:02:21 +0900

    安定性の向上

--- Main.js
+++ Main.js
@@ -1,4 +1,4 @@
-if(document.getElementsByName("message3").length==0) {
+if(document.getElementsByName("message3").length===0 && document.getElementsByName("usr_name").length !== 0) {
     //学籍番号とかの方だったら
     AccountURL = chrome.extension.getURL('Account.txt');
     let AxmlHttp = new XMLHttpRequest();
@@ -21,7 +21,7 @@ if(document.getElementsByName("message3").length==0) {
         document.getElementsByName("OK").item(0).click();
     }
 
-}else {
+}else if(document.getElementsByName("message3").length!==0){
     //マトリクスなら
     var data = 'default';
     var aler = '';
@@ -33,41 +33,47 @@ if(document.getElementsByName("message3").length==0) {
         data = xmlHttp.responseText;
 
         //マトリクスの座標ゲット
-        var tmp = document.getElementsByTagName('th');
+        let tmpo = document.getElementsByTagName('td');
+        for(let i = 0; i<tmpo.length;i++) {
+            console.log(i + ":" + tmpo.item(i).textContent)
+        }
 
         //dataのIndex番号計算
         //英字の番号(0始まり)*7+(数字-1)
         //例：D5 なら，3*7 + (5-1) = 25
-        function getIndexNum(codeNum) {
-            //1に英字，3に数字
-            var eng = tmp.item(codeNum).textContent.toString()[1];
-            var num = tmp.item(codeNum).textContent.toString()[3];
+        function getIndexNum(tmp) {
+            let engIndex = findEng(tmp)
+            let eng = alphabet2int(tmp[engIndex]);
+            let num = tmp[engIndex+2]
             //英字を数字へ変換
-            eng = alphabet2int(eng);
             return eng * 7 + (num - 1);
         }
 
         //英字を数字へ変換
         function alphabet2int(str) {
-            var radix = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
-            var digit = radix.length;
-            if (!/^[A-Z]+$/.test(str)) {
-                throw new Error(str + 'に対する認識可能な数値が見つかりませんでした。');
-            }
-            return parseInt(str.replace(/./g, function (r) {
-                return radix.indexOf(r).toString(digit);
-            }), digit);
+            const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            return alphabet.indexOf(str);
         }
 
-        //引数は4,6,8
-        document.getElementsByName("message3").item(0).value = data[getIndexNum(4)];
-        document.getElementsByName("message4").item(0).value = data[getIndexNum(6)];
-        document.getElementsByName("message5").item(0).value = data[getIndexNum(8)];
-        
+        // 英字を見つける
+        function findEng(tmp) {
+            for(let i = 0; i < tmp.length; i++) {
+                if(alphabet2int(tmp[i]) !== -1) {
+                    return i;
+                }
+            }
+            return 0;
+        }
+
+        //引数は4,5,6
+        let codenum = [4, 5, 6]
+        for(let i = 0; i<3; i++) {
+            document.getElementsByName("message" + (i+3).toString()).item(0).value = data[getIndexNum(tmpo.item(codenum[i]).textContent)];
+        }
+
         //OKをクリック
         document.getElementsByName("OK").item(0).click();
 
-        //window.alert(aler);
+
     }
 }
-
